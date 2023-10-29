2c727fcd606f86191d4617fd894d955b26f974fc
Kayoto0 <148847991+Kayoto0@users.noreply.github.com>
2023-10-29 00:06:39 -0500

    Update word-list.js

--- scripts/word-list.js
+++ scripts/word-list.js
@@ -1,253 +1,254 @@
 const wordList = [
 
     {
-    word: "cell",
-    hint: "The basic structural and functional unit of life."
+      word: "cell",
+      hint: "The basic structural and functional unit of life."
     },
-    
+  
     {
-    word: "molecule",
-    hint: "A group of atoms bonded together."
+      word: "molecule",
+      hint: "A group of atoms bonded together."
     },
-    
+  
     {
-    word: "DNA",
-    hint: "Deoxyribonucleic acid, the genetic material of organisms."
+      word: "DNA",
+      hint: "Deoxyribonucleic acid, the genetic material of organisms."
     },
-    
+  
     {
-    word: "enzymes",
-    hint: "Biological molecules that facilitate chemical reactions."
+      word: "enzymes",
+      hint: "Biological molecules that facilitate chemical reactions."
     },
-    
+  
     {
-    word: "mitosis",
-    hint: "The process of cell division in eukaryotic cells."
+      word: "mitosis",
+      hint: "The process of cell division in eukaryotic cells."
     },
-    
+  
     {
-    word: "photosynthesis",
-    hint: "The process by which plants convert sunlight into energy."
+      word: "photosynthesis",
+      hint: "The process by which plants convert sunlight into energy."
     },
-    
+  
     {
-    word: "bacteria",
-    hint: "Single-celled microorganisms with diverse roles."
+      word: "bacteria",
+      hint: "Single-celled microorganisms with diverse roles."
     },
-    
+  
     {
-    word: "acid",
-    hint: "A substance with a pH less than 7."
+      word: "acid",
+      hint: "A substance with a pH less than 7."
     },
-    
+  
     {
-    word: "evolution",
-    hint: "The change in heritable traits of living organisms over generations."
+      word: "evolution",
+      hint: "The change in heritable traits of living organisms over generations."
     },
-    
+  
     {
-    word: "neurons",
-    hint: "Nerve cells that transmit information in the nervous system."
+      word: "neurons",
+      hint: "Nerve cells that transmit information in the nervous system."
     },
-    
+  
     {
-    word: "ecosystem",
-    hint: "A community of organisms and their environment."
+      word: "ecosystem",
+      hint: "A community of organisms and their environment."
     },
-    
+  
     {
-    word: "elements",
-    hint: "Substances that cannot be chemically broken down."
+      word: "elements",
+      hint: "Substances that cannot be chemically broken down."
     },
-    
+  
     {
-    word: "carbon",
-    hint: "An essential element for life, found in organic compounds."
+      word: "carbon",
+      hint: "An essential element for life, found in organic compounds."
     },
-    
+  
     {
-    word: "genetics",
-    hint: "The study of heredity and genes."
+      word: "genetics",
+      hint: "The study of heredity and genes."
     },
-    
+  
     {
-    word: "atom",
-    hint: "The basic unit of matter."
+      word: "atom",
+      hint: "The basic unit of matter."
     },
-    
+  
     {
-    word: "proteins",
-    hint: "Large molecules critical for the structure and function of cells."
+      word: "proteins",
+      hint: "Large molecules critical for the structure and function of cells."
     },
-    
+  
     {
-    word: "periodic table",
-    hint: "A tabular arrangement of chemical elements."
+      word: "periodictable",
+      hint: "A tabular arrangement of chemicalelements."
     },
-    
+  
     {
-    word: "mitochondria",
-    hint: "Organelles that produce energy in eukaryotic cells."
+      word: "mitochondria",
+      hint: "Organelles that produce energy in eukaryotic cells."
     },
-    
+  
     {
-    word: "genetic code",
-    hint: "The set of rules used by living cells to translate genetic information."
+      word: "geneticcode",
+      hint: "The set of rules used by living cells to translate genetic information."
     },
-    
+  
     {
-    word: "biomolecules",
-    hint: "Complex organic molecules found in living organisms."
+      word: "biomolecules",
+      hint: "Complex organic molecules found in living organisms."
     },
-    
+  
     {
-    word: "hydrogen",
-    hint: "The lightest and most abundant chemical element in the universe."
+      word: "hydrogen",
+      hint: "The lightest and most abundant chemicalelement in the universe."
     },
-    
+  
     {
-    word: "ecology",
-    hint: "The study of interactions between organisms and their environment."
+      word: "ecology",
+      hint: "The study of interactions between organisms and their environment."
     },
-    
+  
     {
-    word: "nucleus",
-    hint: "The central part of an atom and a cell."
+      word: "nucleus",
+      hint: "The central part of an atom and acell."
     },
-    
+  
     {
-    word: "hormones",
-    hint: "Regulatory molecules that control various bodily functions."
+      word: "hormones",
+      hint: "Regulatory molecules that control various bodilyfunctions."
     },
-    
+  
     {
-    word: "chemistry",
-    hint: "The study of matter, its properties, and interactions."
+      word: "chemistry",
+      hint: "The study of matter, itsproperties, and interactions."
     },
-    
+  
     {
-    word: "chlorophyll",
-    hint: "A green pigment essential for photosynthesis."
+      word: "chlorophyll",
+      hint: "A green pigment essential forphotosynthesis."
     },
-    
+  
     {
-    word: "microbiology",
-    hint: "The study of microorganisms and their effects."
+      word: "microbiology",
+      hint: "The study of microorganisms andtheir effects."
     },
-    
+  
     {
-    word: "symbiosis",
-    hint: "A close ecological relationship between two different species."
+      word: "symbiosis",
+      hint: "A close ecological relationship betweentwo different species."
     },
-    
+  
     {
-    word: "isotopes",
-    hint: "Atoms of the same element with different numbers of neutrons."
+      word: "isotopes",
+      hint: "Atoms of the same element withdifferent numbers of neutrons."
     },
-    
+  
     {
-    word: "ecosystem",
-    hint: "A community of organisms and their environment."
+      word: "ecosystem",
+      hint: "A community of organisms and their environment."
     },
-    
+  
     {
-    word: "pollution",
-    hint: "The introduction of contaminants into the environment."
+      word: "pollution",
+      hint: "The introduction of contaminants intothe environment."
     },
-    
+  
     {
-    word: "organic chemistry",
-    hint: "The study of carbon compounds and their reactions."
+      word: "organicchemistry",
+      hint: "The study of carbon compounds andtheir reactions."
     },
-    
+  
     {
-    word: "enzymatic reaction",
-    hint: "A chemical reaction catalyzed by enzymes."
+      word: "enzymaticreaction",
+      hint: "A chemical reaction catalyzed byenzymes."
     },
-    
+  
     {
-    word: "cell membrane",
-    hint: "A semipermeable membrane surrounding the cell."
+      word: "cellmembrane",
+      hint: "A semipermeable membrane surrounding thecell."
     },
-    
+  
     {
-    word: "biotechnology",
-    hint: "The use of living organisms to develop products or processes."
+      word: "biotechnology",
+      hint: "The use of living organisms to developproducts or processes."
     },
-    
+  
     {
-    word: "mitochondrial DNA",
-    hint: "Genetic material found in mitochondria."
+      word: "mitochondrialDNA",
+      hint: "Genetic material found in mitochondria."
     },
-    
+  
     {
-    word: "inorganic chemistry",
-    hint: "The study of inorganic compounds and minerals."
+      word: "inorganicchemistry",
+      hint: "The study of inorganic compounds andminerals."
     },
-    
+  
     {
-    word: "biodiversity",
-    hint: "The variety of life in a particular ecosystem."
+      word: "biodiversity",
+      hint: "The variety of life in a particularecosystem."
     },
-    
+  
     {
-    word: "acid-base reaction",
-    hint: "A chemical reaction between an acid and a base."
+      word: "acid-basereaction",
+      hint: "A chemical reaction between an acid and abase."
     },
-    
+  
     {
-    word: "RNA",
-    hint: "Ribonucleic acid, involved in protein synthesis."
+      word: "RNA",
+      hint: "Ribonucleic acid, involved in proteinsynthesis."
     },
-    
+  
     {
-    word: "entropy",
-    hint: "A measure of disorder or randomness."
+      word: "entropy",
+      hint: "A measure of disorder or randomness."
     },
-    
+  
     {
-    word: "endoplasmic reticulum",
-    hint: "An organelle involved in protein and lipid synthesis."
+      word: "endoplasmicreticulum",
+      hint: "An organelle involved in protein and lipidsynthesis."
     },
-    
+  
     {
-    word: "genome",
-    hint: "The complete set of genes or genetic material present in a cell."
+      word: "genome",
+      hint: "The complete set of genes or geneticmaterial present in a cell."
     },
-    
+  
     {
-    word: "nuclear fusion",
-    hint: "The process by which stars generate light and heat."
+      word: "nuclearfusion",
+      hint: "The process by which stars generate lightand heat."
     },
-    
+  
     {
-    word: "biological clock",
-    hint: "An internal mechanism that controls circadian rhythms."
+      word: "biologicalclock",
+      hint: "An internal mechanism that controls circadianrhythms."
     },
-    
+  
     {
-    word: "recombinant DNA",
-    hint: "DNA molecules created by combining genetic material from multiple sources."
+      word: "recombinantDNA",
+      hint: "DNA molecules created by combining geneticmaterial from multiple sources."
     },
-    
+  
     {
-    word: "toxicology",
-    hint: "The study of the adverse effects of chemicals on living organisms."
+      word: "toxicology",
+      hint: "The study of the adverse effects of chemicalson living organisms."
     },
-    
+  
     {
-    word: "xenobiotics",
-    hint: "Chemical compounds foreign to a living organism."
+      word: "xenobiotics",
+      hint: "Chemical compounds foreign to a livingorganism."
     },
-    
+  
     {
-    word: "neurotransmitter",
-    hint: "Chemicals that transmit signals between nerve cells."
+      word: "neurotransmitter",
+      hint: "Chemicals that transmit signals between nervecells."
     },
-    
+  
     {
-    word: "covalent bond",
-    hint: "A chemical bond where atoms share electron pairs."
+      word: "covalentbond",
+      hint: "A chemical bond where atoms share electronpairs."
     }
-
-];
+  
+  ];
+  
