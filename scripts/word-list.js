c158575db6b41db274c0952b32a7afb21deae69d
Kayoto0 <148847991+Kayoto0@users.noreply.github.com>
2023-10-28 23:17:29 -0500

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,626 @@
+const wordList = [
+{
+word: "cell",
+hint: "The basic structural and functional unit of life."
+},
+
+{
+word: "molecule",
+hint: "A group of atoms bonded together."
+},
+
+{
+word: "DNA",
+hint: "Deoxyribonucleic acid, the genetic material of organisms."
+},
+
+{
+word: "enzymes",
+hint: "Biological molecules that facilitate chemical reactions."
+},
+
+{
+word: "mitosis",
+hint: "The process of cell division in eukaryotic cells."
+},
+
+{
+word: "photosynthesis",
+hint: "The process by which plants convert sunlight into energy."
+},
+
+{
+word: "bacteria",
+hint: "Single-celled microorganisms with diverse roles."
+},
+
+{
+word: "acid",
+hint: "A substance with a pH less than 7."
+},
+
+{
+word: "evolution",
+hint: "The change in heritable traits of living organisms over generations."
+},
+
+{
+word: "neurons",
+hint: "Nerve cells that transmit information in the nervous system."
+},
+
+{
+word: "ecosystem",
+hint: "A community of organisms and their environment."
+},
+
+{
+word: "elements",
+hint: "Substances that cannot be chemically broken down."
+},
+
+{
+word: "carbon",
+hint: "An essential element for life, found in organic compounds."
+},
+
+{
+word: "genetics",
+hint: "The study of heredity and genes."
+},
+
+{
+word: "atom",
+hint: "The basic unit of matter."
+},
+
+{
+word: "proteins",
+hint: "Large molecules critical for the structure and function of cells."
+},
+
+{
+word: "periodic table",
+hint: "A tabular arrangement of chemical elements."
+},
+
+{
+word: "mitochondria",
+hint: "Organelles that produce energy in eukaryotic cells."
+},
+
+{
+word: "genetic code",
+hint: "The set of rules used by living cells to translate genetic information."
+},
+
+{
+word: "biomolecules",
+hint: "Complex organic molecules found in living organisms."
+},
+
+{
+word: "hydrogen",
+hint: "The lightest and most abundant chemical element in the universe."
+},
+
+{
+word: "ecology",
+hint: "The study of interactions between organisms and their environment."
+},
+
+{
+word: "nucleus",
+hint: "The central part of an atom and a cell."
+},
+
+{
+word: "hormones",
+hint: "Regulatory molecules that control various bodily functions."
+},
+
+{
+word: "chemistry",
+hint: "The study of matter, its properties, and interactions."
+},
+
+{
+word: "chlorophyll",
+hint: "A green pigment essential for photosynthesis."
+},
+
+{
+word: "microbiology",
+hint: "The study of microorganisms and their effects."
+},
+
+{
+word: "symbiosis",
+hint: "A close ecological relationship between two different species."
+},
+
+{
+word: "isotopes",
+hint: "Atoms of the same element with different numbers of neutrons."
+},
+
+{
+word: "ecosystem",
+hint: "A community of organisms and their environment."
+},
+
+{
+word: "pollution",
+hint: "The introduction of contaminants into the environment."
+},
+
+{
+word: "organic chemistry",
+hint: "The study of carbon compounds and their reactions."
+},
+
+{
+word: "enzymatic reaction",
+hint: "A chemical reaction catalyzed by enzymes."
+},
+
+{
+word: "cell membrane",
+hint: "A semipermeable membrane surrounding the cell."
+},
+
+{
+word: "biotechnology",
+hint: "The use of living organisms to develop products or processes."
+},
+
+{
+word: "mitochondrial DNA",
+hint: "Genetic material found in mitochondria."
+},
+
+{
+word: "inorganic chemistry",
+hint: "The study of inorganic compounds and minerals."
+},
+
+{
+word: "biodiversity",
+hint: "The variety of life in a particular ecosystem."
+},
+
+{
+word: "acid-base reaction",
+hint: "A chemical reaction between an acid and a base."
+},
+
+{
+word: "RNA",
+hint: "Ribonucleic acid, involved in protein synthesis."
+},
+
+{
+word: "entropy",
+hint: "A measure of disorder or randomness."
+},
+
+{
+word: "endoplasmic reticulum",
+hint: "An organelle involved in protein and lipid synthesis."
+},
+
+{
+word: "genome",
+hint: "The complete set of genes or genetic material present in a cell."
+},
+
+{
+word: "nuclear fusion",
+hint: "The process by which stars generate light and heat."
+},
+
+{
+word: "biological clock",
+hint: "An internal mechanism that controls circadian rhythms."
+},
+
+{
+word: "recombinant DNA",
+hint: "DNA molecules created by combining genetic material from multiple sources."
+},
+
+{
+word: "toxicology",
+hint: "The study of the adverse effects of chemicals on living organisms."
+},
+
+{
+word: "xenobiotics",
+hint: "Chemical compounds foreign to a living organism."
+},
+
+{
+word: "neurotransmitter",
+hint: "Chemicals that transmit signals between nerve cells."
+},
+
+{
+word: "covalent bond",
+hint: "A chemical bond where atoms share electron pairs."
+},
+{
+word: "photosynthesis",
+hint: "The process by which plants convert sunlight into energy."
+},
+{
+word: "bacteria",
+hint: "Single-celled microorganisms with diverse roles."
+},
+{
+word: "acid",
+hint: "A substance with a pH less than 7."
+},
+{
+word: "evolution",
+hint: "The change in heritable traits of living organisms over generations."
+},
+{
+word: "neurons",
+hint: "Nerve cells that transmit information in the nervous system."
+},
+{
+word: "ecosystem",
+hint: "A community of organisms and their environment."
+},
+{
+word: "elements",
+hint: "Substances that cannot be chemically broken down."
+},
+{
+word: "carbon",
+hint: "An essential element for life, found in organic compounds."
+},
+{
+word: "genetics",
+hint: "The study of heredity and genes."
+},
+{
+word: "atom",
+hint: "The basic unit of matter."
+},
+{
+word: "proteins",
+hint: "Large molecules critical for the structure and function of cells."
+},
+{
+word: "periodic table",
+hint: "A tabular arrangement of chemical elements."
+},
+{
+word: "mitochondria",
+hint: "Organelles that produce energy in eukaryotic cells."
+},
+{
+word: "genetic code",
+hint: "The set of rules used by living cells to translate genetic information."
+},
+{
+word: "biomolecules",
+hint: "Complex organic molecules found in living organisms."
+},
+{
+word: "hydrogen",
+hint: "The lightest and most abundant chemical element in the universe."
+},
+{
+word: "ecology",
+hint: "The study of interactions between organisms and their environment."
+},
+{
+word: "nucleus",
+hint: "The central part of an atom and a cell."
+},
+{
+word: "hormones",
+hint: "Regulatory molecules that control various bodily functions."
+},
+{
+word: "chemistry",
+hint: "The study of matter, its properties, and interactions."
+},
+{
+word: "chlorophyll",
+hint: "A green pigment essential for photosynthesis."
+},
+{
+word: "microbiology",
+hint: "The study of microorganisms and their effects."
+},
+{
+word: "symbiosis",
+hint: "A close ecological relationship between two different species."
+},
+{
+word: "isotopes",
+hint: "Atoms of the same element with different numbers of neutrons."
+},
+{
+word: "ecosystem",
+hint: "A community of organisms and their environment."
+},
+{
+word: "pollution",
+hint: "The introduction of contaminants into the environment."
+},
+{
+word: "organic chemistry",
+hint: "The study of carbon compounds and their reactions."
+},
+{
+word: "enzymatic reaction",
+hint: "A chemical reaction catalyzed by enzymes."
+},
+{
+word: "cell membrane",
+hint: "A semipermeable membrane surrounding the cell."
+},
+{
+word: "biotechnology",
+hint: "The use of living organisms to develop products or processes."
+},
+{
+word: "mitochondrial DNA",
+hint: "Genetic material found in mitochondria."
+},
+{
+word: "inorganic chemistry",
+hint: "The study of inorganic compounds and minerals."
+},
+{
+word: "biodiversity",
+hint: "The variety of life in a particular ecosystem."
+},
+{
+word: "acid-base reaction",
+hint: "A chemical reaction between an acid and a base."
+},
+{
+word: "RNA",
+hint: "Ribonucleic acid, involved in protein synthesis."
+},
+{
+word: "entropy",
+hint: "A measure of disorder or randomness."
+},
+{
+word: "endoplasmic reticulum",
+hint: "An organelle involved in protein and lipid synthesis."
+},
+{
+word: "genome",
+hint: "The complete set of genes or genetic material present in a cell."
+},
+{
+word: "nuclear fusion",
+hint: "The process by which stars generate light and heat."
+},
+{
+word: "biological clock",
+hint: "An internal mechanism that controls circadian rhythms."
+},
+{
+word: "recombinant DNA",
+hint: "DNA molecules created by combining genetic material from multiple sources."
+},
+{
+word: "toxicology",
+hint: "The study of the adverse effects of chemicals on living organisms."
+},
+{
+word: "xenobiotics",
+hint: "Chemical compounds foreign to a living organism."
+},
+{
+word: "neurotransmitter",
+hint: "Chemicals that transmit signals between nerve cells."
+},
+{
+word: "covalent bond",
+hint: "A chemical bond where atoms share electron pairs."
+},
+{
+word: "tissue",
+hint: "A group of cells that have a similar structure and function."
+},
+{
+word: "mutation",
+hint: "A change in the DNA sequence of an organism."
+},
+{
+word: "mendelian genetics",
+hint: "The study of how genetic traits are inherited."
+},
+{
+word: "ions",
+hint: "Charged particles formed when atoms gain or lose electrons."
+},
+{
+word: "organic compounds",
+hint: "Compounds containing carbon-hydrogen (C-H) bonds."
+},
+{
+word: "mitochondrial DNA",
+hint: "Genetic material found in the mitochondria."
+},
+{
+word: "homeostasis",
+hint: "The body's ability to maintain a stable internal environment."
+},
+{
+word: "genetic variation",
+hint: "Differences in genetic makeup among individuals."
+},
+{
+word: "biological diversity",
+hint: "Variety and variability of life on Earth."
+},
+{
+word: "scientific method",
+hint: "A systematic approach to solving scientific problems."
+},
+{
+word: "tectonic plates",
+hint: "Large sections of the Earth's lithosphere that move."
+},
+{
+word: "fossils",
+hint: "Preserved remains of ancient organisms."
+},
+{
+word: "nuclear reaction",
+hint: "A process in which atomic nuclei change."
+},
+{
+word: "cytoplasm",
+hint: "The gel-like substance within a cell."
+},
+{
+word: "ecological niche",
+hint: "The role and position of a species in its ecosystem."
+},
+{
+word: "pH scale",
+hint: "A measure of the acidity or alkalinity of a solution."
+},
+{
+word: "genetic engineering",
+hint: "Manipulating an organism's genes for practical purposes."
+},
+{
+word: "natural selection",
+hint: "The process by which organisms with advantageous traits survive and reproduce."
+},
+{
+word: "gravity",
+hint: "The force that attracts two masses toward each other."
+},
+{
+word: "crystal structure",
+hint: "The arrangement of atoms in a crystalline material."
+},
+{
+word: "cell division",
+hint: "The process by which a cell divides into two daughter cells."
+},
+{
+word: "kinetic energy",
+hint: "The energy of an object in motion."
+},
+{
+word: "nuclear energy",
+hint: "Energy released from atomic nuclei."
+},
+{
+word: "diffusion",
+hint: "The movement of particles from an area of high concentration to low concentration."
+},
+{
+word: "epigenetics",
+hint: "Changes in gene expression without altering DNA sequences."
+},
+{
+word: "substrate",
+hint: "The substance upon which an enzyme acts."
+},
+{
+word: "quantum mechanics",
+hint: "A branch of physics dealing with the behavior of very small particles."
+},
+{
+word: "biological macromolecules",
+hint: "Large organic molecules essential for life."
+},
+{
+word: "cellular respiration",
+hint: "The process of converting glucose into energy in cells."
+},
+{
+word: "genome sequencing",
+hint: "Determining the order of DNA nucleotides in an organism's genome."
+},
+{
+word: "vector-borne diseases",
+hint: "Diseases transmitted to humans by vectors like mosquitoes."
+},
+{
+word: "RNA splicing",
+hint: "The removal of non-coding regions from RNA transcripts."
+},
+{
+word: "amino acids",
+hint: "The building blocks of proteins."
+},
+{
+word: "neutron",
+hint: "A subatomic particle found in the nucleus of an atom."
+},
+{
+word: "mutation",
+hint: "A change in the DNA sequence of an organism."
+},
+{
+word: "molecular biology",
+hint: "The study of biological molecules and their interactions."
+},
+{
+word: "nanotechnology",
+hint: "The manipulation of matter at the nanoscale."
+},
+{
+word: "chemical reaction",
+hint: "The process that transforms one substance into another."
+},
+{
+word: "electromagnetism",
+hint: "The force that combines electricity and magnetism."
+},
+{
+word: "gene expression",
+hint: "The process by which information in a gene is used to produce a functional gene product."
+},
+{
+word: "scientific theory",
+hint: "A well-substantiated explanation of some aspect of the natural world."
+},
+{
+word: "oxidation",
+hint: "The loss of electrons during a chemical reaction."
+},
+{
+word: "homeostasis",
+hint: "The body's ability to maintain a stable internal environment."
+},
+{
+word: "ecological succession",
+hint: "The process of change in the species structure of an ecological community."
+},
+{
+word: "fertilization",
+hint: "The fusion of gametes to initiate the development of a new individual organism."
+},
+{
+word: "radiation",
+hint: "The emission of energy as electromagnetic waves or as moving subatomic particles."
+},
+{
+word: "meiosis",
+hint: "A type of cell division that reduces the number of chromosomes in a cell."
+},
+{
+word: "biochemistry",
+hint: "The branch of science that explores the chemical processes within and related to living organisms."
+},
+
+
+
+];
